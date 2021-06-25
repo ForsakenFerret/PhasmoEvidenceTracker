e92c0fce2c4070b5ea6fd55c40830dfedbdc9c76
ForsakenFerret <86497390+ForsakenFerret@users.noreply.github.com>
2021-06-25 16:16:37 -0500

    Add files via upload

---
+++ Phasmo/Phasmo/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿
+namespace Phasmo
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblDemon = new System.Windows.Forms.Label();
+			this.lblBanshee = new System.Windows.Forms.Label();
+			this.lblSpirit = new System.Windows.Forms.Label();
+			this.lblWraith = new System.Windows.Forms.Label();
+			this.lblPhantom = new System.Windows.Forms.Label();
+			this.lblPoltergeist = new System.Windows.Forms.Label();
+			this.lblOni = new System.Windows.Forms.Label();
+			this.lblYurei = new System.Windows.Forms.Label();
+			this.lblShade = new System.Windows.Forms.Label();
+			this.lblRevenant = new System.Windows.Forms.Label();
+			this.lblMare = new System.Windows.Forms.Label();
+			this.lblJinn = new System.Windows.Forms.Label();
+			this.lblYokai = new System.Windows.Forms.Label();
+			this.lblHantu = new System.Windows.Forms.Label();
+			this.cbEMF = new System.Windows.Forms.CheckBox();
+			this.cbFinger = new System.Windows.Forms.CheckBox();
+			this.cbFreezing = new System.Windows.Forms.CheckBox();
+			this.cbOrbs = new System.Windows.Forms.CheckBox();
+			this.cbWriting = new System.Windows.Forms.CheckBox();
+			this.cbSpiritBox = new System.Windows.Forms.CheckBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// lblDemon
+			// 
+			this.lblDemon.AutoSize = true;
+			this.lblDemon.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblDemon.Location = new System.Drawing.Point(215, 65);
+			this.lblDemon.Name = "lblDemon";
+			this.lblDemon.Size = new System.Drawing.Size(70, 26);
+			this.lblDemon.TabIndex = 6;
+			this.lblDemon.Text = "Demon";
+			// 
+			// lblBanshee
+			// 
+			this.lblBanshee.AutoSize = true;
+			this.lblBanshee.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblBanshee.Location = new System.Drawing.Point(215, 22);
+			this.lblBanshee.Name = "lblBanshee";
+			this.lblBanshee.Size = new System.Drawing.Size(83, 26);
+			this.lblBanshee.TabIndex = 7;
+			this.lblBanshee.Text = "Banshee";
+			// 
+			// lblSpirit
+			// 
+			this.lblSpirit.AutoSize = true;
+			this.lblSpirit.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblSpirit.Location = new System.Drawing.Point(601, 22);
+			this.lblSpirit.Name = "lblSpirit";
+			this.lblSpirit.Size = new System.Drawing.Size(53, 26);
+			this.lblSpirit.TabIndex = 8;
+			this.lblSpirit.Text = "Spirit";
+			// 
+			// lblWraith
+			// 
+			this.lblWraith.AutoSize = true;
+			this.lblWraith.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblWraith.Location = new System.Drawing.Point(601, 108);
+			this.lblWraith.Name = "lblWraith";
+			this.lblWraith.Size = new System.Drawing.Size(64, 26);
+			this.lblWraith.TabIndex = 9;
+			this.lblWraith.Text = "Wraith";
+			// 
+			// lblPhantom
+			// 
+			this.lblPhantom.AutoSize = true;
+			this.lblPhantom.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblPhantom.Location = new System.Drawing.Point(459, 22);
+			this.lblPhantom.Name = "lblPhantom";
+			this.lblPhantom.Size = new System.Drawing.Size(84, 26);
+			this.lblPhantom.TabIndex = 10;
+			this.lblPhantom.Text = "Phantom";
+			// 
+			// lblPoltergeist
+			// 
+			this.lblPoltergeist.AutoSize = true;
+			this.lblPoltergeist.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblPoltergeist.Location = new System.Drawing.Point(459, 65);
+			this.lblPoltergeist.Name = "lblPoltergeist";
+			this.lblPoltergeist.Size = new System.Drawing.Size(97, 26);
+			this.lblPoltergeist.TabIndex = 11;
+			this.lblPoltergeist.Text = "Poltergeist";
+			// 
+			// lblOni
+			// 
+			this.lblOni.AutoSize = true;
+			this.lblOni.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblOni.Location = new System.Drawing.Point(356, 108);
+			this.lblOni.Name = "lblOni";
+			this.lblOni.Size = new System.Drawing.Size(40, 26);
+			this.lblOni.TabIndex = 17;
+			this.lblOni.Text = "Oni";
+			// 
+			// lblYurei
+			// 
+			this.lblYurei.AutoSize = true;
+			this.lblYurei.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblYurei.Location = new System.Drawing.Point(712, 65);
+			this.lblYurei.Name = "lblYurei";
+			this.lblYurei.Size = new System.Drawing.Size(54, 26);
+			this.lblYurei.TabIndex = 16;
+			this.lblYurei.Text = "Yurei";
+			// 
+			// lblShade
+			// 
+			this.lblShade.AutoSize = true;
+			this.lblShade.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblShade.Location = new System.Drawing.Point(601, 65);
+			this.lblShade.Name = "lblShade";
+			this.lblShade.Size = new System.Drawing.Size(64, 26);
+			this.lblShade.TabIndex = 15;
+			this.lblShade.Text = "Shade";
+			// 
+			// lblRevenant
+			// 
+			this.lblRevenant.AutoSize = true;
+			this.lblRevenant.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblRevenant.Location = new System.Drawing.Point(459, 108);
+			this.lblRevenant.Name = "lblRevenant";
+			this.lblRevenant.Size = new System.Drawing.Size(89, 26);
+			this.lblRevenant.TabIndex = 14;
+			this.lblRevenant.Text = "Revenant";
+			// 
+			// lblMare
+			// 
+			this.lblMare.AutoSize = true;
+			this.lblMare.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblMare.Location = new System.Drawing.Point(356, 65);
+			this.lblMare.Name = "lblMare";
+			this.lblMare.Size = new System.Drawing.Size(53, 26);
+			this.lblMare.TabIndex = 13;
+			this.lblMare.Text = "Mare";
+			// 
+			// lblJinn
+			// 
+			this.lblJinn.AutoSize = true;
+			this.lblJinn.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblJinn.Location = new System.Drawing.Point(356, 22);
+			this.lblJinn.Name = "lblJinn";
+			this.lblJinn.Size = new System.Drawing.Size(45, 26);
+			this.lblJinn.TabIndex = 12;
+			this.lblJinn.Text = "Jinn";
+			// 
+			// lblYokai
+			// 
+			this.lblYokai.AutoSize = true;
+			this.lblYokai.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblYokai.Location = new System.Drawing.Point(712, 22);
+			this.lblYokai.Name = "lblYokai";
+			this.lblYokai.Size = new System.Drawing.Size(57, 26);
+			this.lblYokai.TabIndex = 21;
+			this.lblYokai.Text = "Yokai";
+			// 
+			// lblHantu
+			// 
+			this.lblHantu.AutoSize = true;
+			this.lblHantu.Font = new System.Drawing.Font("Arial Narrow", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.lblHantu.Location = new System.Drawing.Point(215, 108);
+			this.lblHantu.Name = "lblHantu";
+			this.lblHantu.Size = new System.Drawing.Size(60, 26);
+			this.lblHantu.TabIndex = 22;
+			this.lblHantu.Text = "Hantu";
+			// 
+			// cbEMF
+			// 
+			this.cbEMF.AutoSize = true;
+			this.cbEMF.BackColor = System.Drawing.SystemColors.Window;
+			this.cbEMF.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.cbEMF.Location = new System.Drawing.Point(13, 13);
+			this.cbEMF.Name = "cbEMF";
+			this.cbEMF.Size = new System.Drawing.Size(120, 24);
+			this.cbEMF.TabIndex = 23;
+			this.cbEMF.Text = "EMF Level 5";
+			this.cbEMF.UseVisualStyleBackColor = false;
+			this.cbEMF.CheckedChanged += new System.EventHandler(this.cbEMF_CheckedChanged);
+			// 
+			// cbFinger
+			// 
+			this.cbFinger.AutoSize = true;
+			this.cbFinger.BackColor = System.Drawing.SystemColors.Window;
+			this.cbFinger.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.cbFinger.Location = new System.Drawing.Point(13, 37);
+			this.cbFinger.Name = "cbFinger";
+			this.cbFinger.Size = new System.Drawing.Size(143, 24);
+			this.cbFinger.TabIndex = 24;
+			this.cbFinger.Text = "Fingerprints";
+			this.cbFinger.UseVisualStyleBackColor = false;
+			this.cbFinger.CheckedChanged += new System.EventHandler(this.cbFinger_CheckedChanged);
+			// 
+			// cbFreezing
+			// 
+			this.cbFreezing.AutoSize = true;
+			this.cbFreezing.BackColor = System.Drawing.SystemColors.Window;
+			this.cbFreezing.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.cbFreezing.Location = new System.Drawing.Point(13, 61);
+			this.cbFreezing.Name = "cbFreezing";
+			this.cbFreezing.Size = new System.Drawing.Size(161, 24);
+			this.cbFreezing.TabIndex = 25;
+			this.cbFreezing.Text = "Freezing Temps";
+			this.cbFreezing.UseVisualStyleBackColor = false;
+			this.cbFreezing.CheckedChanged += new System.EventHandler(this.cbFreezing_CheckedChanged);
+			// 
+			// cbOrbs
+			// 
+			this.cbOrbs.AutoSize = true;
+			this.cbOrbs.BackColor = System.Drawing.SystemColors.Window;
+			this.cbOrbs.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.cbOrbs.Location = new System.Drawing.Point(13, 85);
+			this.cbOrbs.Name = "cbOrbs";
+			this.cbOrbs.Size = new System.Drawing.Size(128, 24);
+			this.cbOrbs.TabIndex = 26;
+			this.cbOrbs.Text = "Ghost Orbs";
+			this.cbOrbs.UseVisualStyleBackColor = false;
+			this.cbOrbs.CheckedChanged += new System.EventHandler(this.cbOrbs_CheckedChanged);
+			// 
+			// cbWriting
+			// 
+			this.cbWriting.AutoSize = true;
+			this.cbWriting.BackColor = System.Drawing.SystemColors.Window;
+			this.cbWriting.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.cbWriting.Location = new System.Drawing.Point(12, 109);
+			this.cbWriting.Name = "cbWriting";
+			this.cbWriting.Size = new System.Drawing.Size(157, 24);
+			this.cbWriting.TabIndex = 27;
+			this.cbWriting.Text = "Ghost Writing";
+			this.cbWriting.UseVisualStyleBackColor = false;
+			this.cbWriting.CheckedChanged += new System.EventHandler(this.cbWriting_CheckedChanged);
+			// 
+			// cbSpiritBox
+			// 
+			this.cbSpiritBox.AutoSize = true;
+			this.cbSpiritBox.BackColor = System.Drawing.SystemColors.Window;
+			this.cbSpiritBox.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.cbSpiritBox.Location = new System.Drawing.Point(12, 133);
+			this.cbSpiritBox.Name = "cbSpiritBox";
+			this.cbSpiritBox.Size = new System.Drawing.Size(119, 24);
+			this.cbSpiritBox.TabIndex = 28;
+			this.cbSpiritBox.Text = "Spirit Box";
+			this.cbSpiritBox.UseVisualStyleBackColor = false;
+			this.cbSpiritBox.CheckedChanged += new System.EventHandler(this.cbSpiritBox_CheckedChanged);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(3, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(190, 153);
+			this.pictureBox1.TabIndex = 29;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(803, 168);
+			this.Controls.Add(this.cbSpiritBox);
+			this.Controls.Add(this.cbWriting);
+			this.Controls.Add(this.cbOrbs);
+			this.Controls.Add(this.cbFreezing);
+			this.Controls.Add(this.cbFinger);
+			this.Controls.Add(this.cbEMF);
+			this.Controls.Add(this.lblHantu);
+			this.Controls.Add(this.lblYokai);
+			this.Controls.Add(this.lblOni);
+			this.Controls.Add(this.lblYurei);
+			this.Controls.Add(this.lblShade);
+			this.Controls.Add(this.lblRevenant);
+			this.Controls.Add(this.lblMare);
+			this.Controls.Add(this.lblJinn);
+			this.Controls.Add(this.lblPoltergeist);
+			this.Controls.Add(this.lblPhantom);
+			this.Controls.Add(this.lblWraith);
+			this.Controls.Add(this.lblSpirit);
+			this.Controls.Add(this.lblBanshee);
+			this.Controls.Add(this.lblDemon);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Phamso Evidence Recorder";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label lblDemon;
+		private System.Windows.Forms.Label lblBanshee;
+		private System.Windows.Forms.Label lblSpirit;
+		private System.Windows.Forms.Label lblWraith;
+		private System.Windows.Forms.Label lblPhantom;
+		private System.Windows.Forms.Label lblPoltergeist;
+		private System.Windows.Forms.Label lblOni;
+		private System.Windows.Forms.Label lblYurei;
+		private System.Windows.Forms.Label lblShade;
+		private System.Windows.Forms.Label lblRevenant;
+		private System.Windows.Forms.Label lblMare;
+		private System.Windows.Forms.Label lblJinn;
+		private System.Windows.Forms.Label lblYokai;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lblHantu;
+		private System.Windows.Forms.CheckBox cbEMF;
+		private System.Windows.Forms.CheckBox cbFinger;
+		private System.Windows.Forms.CheckBox cbFreezing;
+		private System.Windows.Forms.CheckBox cbOrbs;
+		private System.Windows.Forms.CheckBox cbWriting;
+		private System.Windows.Forms.CheckBox cbSpiritBox;
+		private System.Windows.Forms.PictureBox pictureBox1;
+	}
+}
+
---
+++ Phasmo/Phasmo/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phasmo
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void cbEMF_CheckedChanged(object sender, EventArgs e)
+		{
+			UpdateGhostList();
+		}
+
+		private void cbFinger_CheckedChanged(object sender, EventArgs e)
+		{
+			UpdateGhostList();
+		}
+
+		private void cbFreezing_CheckedChanged(object sender, EventArgs e)
+		{
+			UpdateGhostList();
+		}
+
+		private void cbOrbs_CheckedChanged(object sender, EventArgs e)
+		{
+			UpdateGhostList();
+		}
+
+		private void cbWriting_CheckedChanged(object sender, EventArgs e)
+		{
+			UpdateGhostList();
+		}
+
+		private void cbSpiritBox_CheckedChanged(object sender, EventArgs e)
+		{
+			UpdateGhostList();
+		}
+
+
+		private void UpdateGhostList()
+		{
+			// reset the table
+			lblBanshee.Visible = true;
+			lblDemon.Visible = true;
+			lblHantu.Visible = true;
+			lblJinn.Visible = true;
+			lblMare.Visible = true;
+			lblOni.Visible = true;
+			lblPhantom.Visible = true;
+			lblPoltergeist.Visible = true;
+			lblRevenant.Visible = true;
+			lblShade.Visible = true;
+			lblSpirit.Visible = true;
+			lblWraith.Visible = true;
+			lblYokai.Visible = true;
+			lblYurei.Visible = true;
+			cbEMF.Visible = true;
+			cbFinger.Visible = true;
+			cbOrbs.Visible = true;
+			cbFreezing.Visible = true;
+			cbOrbs.Visible = true;
+			cbWriting.Visible = true;
+			cbSpiritBox.Visible = true;
+
+			// set checkSum
+			int checkSum = 0;
+
+			// if EMF 5 is checked 
+			if (cbEMF.Checked)
+			{
+				lblDemon.Visible = false;
+				lblHantu.Visible = false;
+				lblMare.Visible = false;
+				lblPoltergeist.Visible = false;
+				lblSpirit.Visible = false;
+				lblWraith.Visible = false;
+				lblYokai.Visible = false;
+				lblYurei.Visible = false;
+				checkSum += 1;
+			}
+
+			// if fingerprints is checked
+			if (cbFinger.Checked)
+			{
+				lblDemon.Visible = false;
+				lblJinn.Visible = false;
+				lblMare.Visible = false;
+				lblOni.Visible = false;
+				lblPhantom.Visible = false;
+				lblShade.Visible = false;
+				lblYokai.Visible = false;
+				lblYurei.Visible = false;
+				checkSum += 2;
+			}
+
+			// if freezing temps are checked
+			if (cbFreezing.Checked)
+			{
+				lblHantu.Visible = false;
+				lblJinn.Visible = false;
+				lblOni.Visible = false;
+				lblPoltergeist.Visible = false;
+				lblRevenant.Visible = false;
+				lblShade.Visible = false;
+				lblSpirit.Visible = false;
+				lblYokai.Visible = false;
+				checkSum += 4;
+			}
+
+			// if ghost orbs are checked
+			if (cbOrbs.Checked)
+			{
+				lblBanshee.Visible = false;
+				lblDemon.Visible = false;
+				lblOni.Visible = false;
+				lblRevenant.Visible = false;
+				lblSpirit.Visible = false;
+				lblWraith.Visible = false;
+				checkSum += 8;
+			}
+
+			// if ghost writing is checked
+			if (cbWriting.Checked)
+			{
+				lblBanshee.Visible = false;
+				lblJinn.Visible = false;
+				lblMare.Visible = false;
+				lblPhantom.Visible = false;
+				lblPoltergeist.Visible = false;
+				lblWraith.Visible = false;
+				checkSum += 16;
+			}
+
+			// if spirit box is checked
+			if (cbSpiritBox.Checked)
+			{
+				lblBanshee.Visible = false;
+				lblHantu.Visible = false;
+				lblPhantom.Visible = false;
+				lblRevenant.Visible = false;
+				lblShade.Visible = false;
+				lblYurei.Visible = false;
+				checkSum += 32;
+			}
+
+			// remove evidence choices
+			// 
+			switch (checkSum)
+			{
+				case 3:
+					cbOrbs.Visible = false;
+					cbSpiritBox.Visible =false;
+					break;
+
+				case 5:
+					cbWriting.Visible = false;
+					cbSpiritBox.Visible = false;
+					break;
+
+				case 6:
+					cbOrbs.Visible = false;
+					cbWriting.Visible = false;
+					break;
+
+				case 7: //banshee
+					cbOrbs.Visible = false;
+					cbWriting.Visible = false;
+					cbSpiritBox.Visible = false;
+					break;
+
+				case 9:
+					cbFinger.Visible = false;
+					break;
+
+				case 10:
+					cbEMF.Visible = false;
+					cbFreezing.Visible = false;
+					break;
+
+				case 12:
+					cbFinger.Visible = false;
+					break;
+
+				case 13: //phantom
+					cbFinger.Visible = false;
+					cbWriting.Visible = false;
+					cbSpiritBox.Visible = false;
+					break;
+
+				case 17:
+					cbFreezing.Visible = false;
+					break;
+
+				case 18:
+					cbFreezing.Visible = false;
+					break;
+
+				case 19: //revenent
+					cbFreezing.Visible = false;
+					cbOrbs.Visible = false;
+					cbSpiritBox.Visible = false;
+					break;
+
+				case 20:
+					cbEMF.Visible = false;
+					cbFinger.Visible = false;
+					break;
+
+				case 25: //shade
+					cbFinger.Visible = false;
+					cbFreezing.Visible = false;
+					cbSpiritBox.Visible = false;
+					break;
+
+				case 26: //hantu
+					cbEMF.Visible = false;
+					cbFreezing.Visible = false;
+					cbSpiritBox.Visible = false;
+					break;
+
+				case 28: //yurei
+					cbEMF.Visible = false;
+					cbFinger.Visible = false;
+					cbSpiritBox.Visible = false;
+					break;
+
+				case 33:
+					cbFinger.Visible = false;
+					cbFreezing.Visible = false;
+					break;
+
+				case 34:
+					cbEMF.Visible = false;
+					break;
+
+				case 36:
+					cbEMF.Visible = false;
+					break;
+
+				case 38: //wraith
+					cbEMF.Visible = false;
+					cbOrbs.Visible = false;
+					cbWriting.Visible = false;
+					break;
+
+				case 40:
+					cbEMF.Visible = false;
+					break;
+
+				case 41: //jinn
+					cbFinger.Visible = false;
+					cbFreezing.Visible = false;
+					cbWriting.Visible = false;
+					break;
+
+				case 42: //poltergeist
+					cbEMF.Visible = false;
+					cbFreezing.Visible = false;
+					cbWriting.Visible = false;
+					break;
+
+				case 44: //mare
+					cbEMF.Visible = false;
+					cbFinger.Visible = false;
+					cbWriting.Visible = false;
+					break;
+
+				case 49: //oni
+					cbFinger.Visible = false;
+					cbFreezing.Visible = false;
+					cbOrbs.Visible = false;
+					break;
+
+				case 50: // spirit
+					cbEMF.Visible = false;
+					cbFreezing.Visible = false;
+					cbOrbs.Visible = false;
+					break;
+
+				case 52: //demon
+					cbEMF.Visible = false;
+					cbFinger.Visible = false;
+					cbOrbs.Visible = false;
+					break;
+
+				case 56: //yokai
+					cbEMF.Visible = false;
+					cbFinger.Visible = false;
+					cbFreezing.Visible = false;
+					break;
+
+			}
+		}
+
+	}
+}
